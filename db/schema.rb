8967ac9abd2ce0774a06328a815b71718267efb3
Heng Desmond <desmond@dsrecruitment.sg>
2017-03-28 15:39:28 +0800

    added comments model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170328065821) do
+ActiveRecord::Schema.define(version: 20170328073907) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "user_name"
+    t.text     "body"
+    t.integer  "helper_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "helpers", force: :cascade do |t|
     t.string   "name"
