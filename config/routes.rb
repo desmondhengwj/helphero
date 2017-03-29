382a4d6cc717c320066b2840abfaf81ed43ea75d
Heng Desmond <desmond@dsrecruitment.sg>
2017-03-29 12:16:40 +0800

    added homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  get 'static/homepage'
+
   devise_for :users
   get 'pages/info'
   resources :helpers do
     resources :comments
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: redirect('/helpers')
+  root 'static#homepage'
 
 end
